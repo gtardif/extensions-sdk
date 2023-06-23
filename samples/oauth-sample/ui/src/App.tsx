46d4ae3f9c6ed24e96b6f32b61d7ff6036ecbfe0
Guillaume Tardif <guillaume.tardif@gmail.com>
2023-06-23 10:51:51 +0200

    Trying to run PKCE SSO with manual code
    Signed-off-by: Guillaume Tardif <guillaume.tardif@gmail.com>

--- samples/oauth-sample/ui/src/App.tsx
+++ samples/oauth-sample/ui/src/App.tsx
@@ -5,12 +5,65 @@ import { Stack, TextField, Typography } from "@mui/material";
 
 const client = createDockerDesktopClient();
 
-const client_id = "xxxx";
+const client_id = "Oxuixl3g21aBbJMiWySpjlxUK3qpnUEW";
 const client_secret = "xxxx";
 
 function useDockerDesktopClient() {
   return client;
 }
+
+/*
+function base64URLEncode(str: any) {
+  return str
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "");
+}
+const verifier = base64URLEncode(crypto.lib.WordArray.random(32));
+
+function sha256(buffer: any) {
+  return crypto.createHash("sha256").update(buffer).digest();
+}
+const challenge = base64URLEncode(sha256(verifier));
+*/
+
+function randomCode(): string {
+  let array = new Uint8Array(64);
+  array = globalThis.crypto.getRandomValues(array);
+  return String.fromCharCode.apply(null, Array.from(array));
+}
+
+function base64URLEncode(str: string): string {
+  const b64 = btoa(str);
+  const encoded = b64.replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
+  return encoded;
+}
+
+const verifier = base64URLEncode(randomCode());
+
+const bufferToBase64UrlEncoded = (hash: ArrayBuffer): string => {
+  const uintArray = new Uint8Array(hash);
+  const numberArray = Array.from(uintArray);
+  const hashString = String.fromCharCode(...numberArray);
+  return urlEncodeB64(btoa(hashString));
+};
+
+const urlEncodeB64 = (input: string) => {
+  const b64Chars: { [index: string]: string } = { "+": "-", "/": "_", "=": "" };
+  return input.replace(/[+/=]/g, (m: string) => b64Chars[m]);
+};
+
+const sha256 = async (str: string): Promise<string> => {
+  const digestOp = await crypto.subtle.digest(
+    { name: "SHA-256" },
+    new TextEncoder().encode(str)
+  );
+  return bufferToBase64UrlEncoded(digestOp);
+};
+
+const redirectUri =
+  "https%3A%2F%2Fopen.docker.com%2Fdashboard%2Fextension-tab%3FextensionId%3Ddocker%2Fsso-sample-extension";
 
 export function App() {
   const [response, setResponse] = React.useState<string>();
@@ -24,23 +77,34 @@ export function App() {
     console.log("processing POST with code " + queryParams.get("code"));
     const requestOptions = {
       method: "POST",
+      Headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
     };
-    fetch(
-      `https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&code=${queryParams.get(
-        "code"
-      )}`,
-      requestOptions
-    )
-      .then((response) => response.text())
+    const tokenRequest = `https://login-stage.docker.com/oauth/token?client_id=${client_id}&code_verifier=${verifier}&grant_type=authorization_code&code=${queryParams.get(
+      "code"
+    )}&redirect_uri=${redirectUri}`;
+    console.log("tokenRequest: ", tokenRequest);
+    fetch(tokenRequest, requestOptions)
+      .then((response) => {
+        console.log("response: " + response);
+        return response.text();
+      })
       .then((data) => {
+        console.log("data: " + data);
         setLoggedIn(true);
         setResponse(data);
       });
   }
 
   const login = async () => {
+    const challenge = await sha256(verifier);
+
+    console.log("challenge: ", challenge);
+    console.log("verifier: ", verifier);
+
     ddClient.host.openExternal(
-      `https://github.com/login/oauth/authorize?client_id=${client_id}`
+      `https://login-stage.docker.com/oauth/authorize?client_id=${client_id}&code_challenge=${challenge}&code_challenge_method=S256&response_type=code&response_mode=query&redirect_uri=${redirectUri}`
     );
   };
 
